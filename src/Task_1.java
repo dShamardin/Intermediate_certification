f8341870dd83179957fa6b0d5579bd7234b2fcfc
DESKTOP-PMQOTRO\Николай <dmitriyrostov2016@gmail.com>
2023-03-01 19:59:47 +0300

    commit

--- src/Task_1.java
+++ src/Task_1.java
@@ -1,27 +1,48 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+
 
 public class Task_1 {
 
     public static void main(String[] args) {
 
+
+
         int word = 15; // количество букв в признании
         int spaces = 3; // количество пробелов в признании
-        int each3word = 62; // количестов сантиметров длины забора на каждые 3 буквы
-        int Lspaces = 12; // количество сантиметров в одном пробеле
-        int Allwords = (15 / 3) * 62; // длина всех букв
-        int Allspaces = 12 * 3; // Длина всех пробелов
-        int confession = Allspaces + Allwords; // Длина признания в сантиметрах
+        int Length3Word = 62; // количестов сантиметров длины забора на каждые 3 буквы
+        int LSpaces = 12; // количество сантиметров в одном пробеле
+        int AllWords = (15 / 3) * 62; // длина всех букв
+        int AllSpaces = 12 * 3; // длина всех пробелов
+        int confession = AllSpaces + AllWords; // Длина признания в сантиметрах
 
-        System.out.println("Введите длину забора в сантиметрах");
+        System.out.println("Введите число сантиметров");
 
-        Scanner sc = new Scanner(System.in);
-        int L = sc.nextInt();
-        if (L < confession) {
-            System.out.println(" Длины забора недостаточно для надписи в признании любви");
-        } else {
-            System.out.println("Длины забора достаточно для написания признания в любви");
+        try {
+            Scanner sc = new Scanner(System.in);
+            int L = sc.nextInt();
+            if(L<confession) {
+                do {
+                    //  for (int L = sc.nextInt(); L < confession; L++){
+                    System.out.println(" Длины забора недостаточно для надписи в признании любви");
+                    System.out.println("");
+                    System.out.println("Введите длину забора в сантиметрах повторно");
+                    break;
+                }
+                while (L>=confession);
+                   }
+            else { System.out.println("Длины забора достаточно");
+            }
+            sc.close();
+             }
+        catch (InputMismatchException e) {
+            System.out.println("Введено не число");
         }
-        sc.close();
+      finally {
+            System.out.println(" ");
+            System.out.println("Дейстие программы завершено");
+        }
     }
 }
 
---
+++ src/ru/Task2/Main.java
@@ -0,0 +1,10 @@
+package ru.Task2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Cactus cactus = new Cactus();
+    }
+}
