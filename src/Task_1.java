ead42460b334e020de0dab4ff350369cfb1d4e04
DESKTOP-PMQOTRO\Николай <dmitriyrostov2016@gmail.com>
2023-03-07 22:52:19 +0300

    Commit

--- src/Task_1.java
+++ src/Task_1.java
@@ -17,25 +17,20 @@ public class Task_1 {
         int AllSpaces = 12 * 3; // длина всех пробелов
         int confession = AllSpaces + AllWords; // Длина признания в сантиметрах
 
+
         System.out.println("Введите число сантиметров");
 
         try {
             Scanner sc = new Scanner(System.in);
-            int L = sc.nextInt();
-            if(L<confession) {
-                do {
-                    //  for (int L = sc.nextInt(); L < confession; L++){
-                    System.out.println(" Длины забора недостаточно для надписи в признании любви");
-                    System.out.println("");
-                    System.out.println("Введите длину забора в сантиметрах повторно");
-                    break;
-                }
-                while (L>=confession);
-                   }
-            else { System.out.println("Длины забора достаточно");
+            int L = 0;
+            do {
+                L = sc.nextInt();
+                System.out.println(" Длины забора недостаточно для надписи в признании любви");
+                System.out.println("");
             }
+            while (L<confession);
             sc.close();
-             }
+        }
         catch (InputMismatchException e) {
             System.out.println("Введено не число");
         }
